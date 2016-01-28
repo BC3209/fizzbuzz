240cd3de69d1aa6150013db61d228c76240c7dc0
BC3209 <bchidoro9@gmail.com>
2016-01-28 16:06:51 -0500

    :white_check_mark: test (rspec): used rspec for TDD Started working with TDD -wrote my tests first -wrote my program based on my tests

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,35 @@
-for num in 1..100
-  if num % 15 == 0
-    puts 'fizzbuzz'
-  elsif num % 5 == 0
-    puts 'buzz'
-  elsif num % 3 == 0
-    puts 'fizz'
+def fizzbuzz(number)
+  fizz(
+    buzz(
+      fizzbuzzy(number)
+    )
+  )
+end
+
+def fizz(num)
+  if num % 3 == 0
+    return 'fizz'
   else
-    puts num
+    num
   end
+end
+
+def buzz(num)
+  if num % 5 == 0
+    return 'buzz'
+  else
+    num
+  end
+end
+
+def fizzbuzzy(num)
+  if num % 15 == 0
+    return 'fizzbuzz'
+  else
+    num
+  end
+end
+
+for num in 1..100
+  puts fizzbuzz(num)
 end
---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,23 @@
+require "spec_helper"
+require_relative "../fizzbuzz"
+
+describe 'fizzbuzz' do
+  it 'prints a value' do
+    expect(fizzbuzz('')).not_to eq(nil)
+  end
+  it 'if given 1, prints 1' do
+    expect(fizzbuzz(1)).to eq(1)
+  end
+  it 'if given 2, prints 2' do
+    expect(fizzbuzz(2)).to eq(2)
+  end
+  it 'if given 3, prints fizz' do
+    expect(fizzbuzz(3)).to eq("fizz")
+  end
+  it 'if given 5, prints buzz' do
+    expect(fizzbuzz(5)).to eq("buzz")
+  end
+  it 'if given 15, prints fizzbuzz' do
+    expect(fizzbuzz(15)).to eq("fizzbuzz")
+  end
+end
